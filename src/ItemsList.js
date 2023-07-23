8655f2074f53607f2ab09c06d37ceb4c9ef98af6
Pavel <web2cap@gmail.com>
2023-07-23 19:31:07 +0200

    refactor line item

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -1,25 +1,14 @@
-import { FaTrashAlt } from 'react-icons/fa'
+import LineItem from "./LineItem"
 
 const ItemsList = ({ items, handleCheck, handleDelete }) => {
     return (
         <ul>
             {items.map((item) => (
-                <li className='item' key={item.id}>
-                    <input
-                        type='checkbox'
-                        onChange={() => handleCheck(item.id)}
-                        checked={item.checked}
-                    />
-                    <label
-                        style={(item.checked) ? { textDecoration: 'line-through' } : null}
-                        onDoubleClick={() => handleCheck(item.id)}
-                    >{item.item}</label>
-                    <FaTrashAlt
-                        onClick={() => handleDelete(item.id)}
-                        role='button'
-                        tabIndex='0'
-                    />
-                </li>
+                <LineItem
+                    item={item}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ))}
         </ul>
     )
