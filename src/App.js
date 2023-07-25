d22d4b0bcea327b0dbaecede5695f706f69c99c7
Pavel <web2cap@gmail.com>
2023-07-25 14:14:46 +0200

    addItem, load from localStorge

--- src/App.js
+++ src/App.js
@@ -3,42 +3,55 @@ import { useState } from 'react';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import AddItem from './AddItem';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "i1"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "i2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "i3"
-    },
-  ])
+  const [items, setItems] = useState(JSON.parse(
+    localStorage.getItem('shoppingList')
+  ))
+
+  const [newItem, setNewItem] = useState('')
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems)
+    localStorage.setItem('shoppingList', JSON.stringify(newItems))
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1
+    const addingNewItem = { id, checked: false, item }
+    const listItems = [...items, addingNewItem]
+    setAndSaveItems(listItems)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!newItem) return
+    addItem(newItem)
+    setNewItem('')
+  }
+
+
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? {
       ...item, checked: !item.checked
     } : item)
-    setItems(listItems)
-    localStorage.setItem('shoppingList', JSON.stringify(listItems))
+    setAndSaveItems(listItems)
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
-    setItems(listItems)
-    localStorage.setItem('shoppingList', JSON.stringify(listItems))
+    setAndSaveItems(listItems)
   }
 
   return (
     <div className="App">
       <Header title="Groceries" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
