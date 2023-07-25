f47c8fba421cc96e3cd9881a8a8259d2552c9507
Pavel <web2cap@gmail.com>
2023-07-25 14:42:34 +0200

    fix add btn

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,11 +1,14 @@
+import { useRef } from "react"
 import { FaPlus } from "react-icons/fa"
 
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+    const inputRef = useRef()
     return (
         <form className='addForm' onSubmit={handleSubmit}>
             <label htmlFor='addItem'>Add Item</label>
             <input
                 autoFocus
+                ref={inputRef}
                 id='addItem'
                 type='text'
                 placeholder='Add Item'
@@ -16,6 +19,7 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
             <button
                 type='submit'
                 aria-label='Add Item'
+                onClick={() => inputRef.current.focus()}
             ><FaPlus /></button>
 
         </form>
